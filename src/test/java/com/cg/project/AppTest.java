2e1d38448c26da460f84bde1a9cc8d4d6c68fee8
pushpaDe <pushpadeshpande111@gmail.com>
2020-09-02 11:51:44 +0530

    initial

---
+++ src/test/java/com/cg/project/AppTest.java
@@ -0,0 +1,24 @@
+package com.cg.project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+    public void test1() {
+        Assert.fail("test1 has Fail");
+    }
+    @Test
+    public void test2() {
+    }
+    @Test
+    public void test3() {
+        Assert.fail("test3 has Fail");
+    }
+}
